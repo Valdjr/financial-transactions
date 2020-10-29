04dc2eb92736a6917a2c1d80989ed05c56c40a0d
valdljr <valdir.lago@bling.com.br>
2020-10-28 22:27:28 -0300

    feat: get and post transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,34 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((acumulator, current) => {
+      if (current.type === 'income') {
+        return acumulator + current.value;
+      }
+      return acumulator;
+    }, 0);
+    const outcome = this.transactions.reduce((acumulator, current) => {
+      if (current.type === 'outcome') {
+        return acumulator + current.value;
+      }
+      return acumulator;
+    }, 0);
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const newTransaction = new Transaction({ title, value, type });
+    this.transactions.push(newTransaction);
+    return newTransaction;
   }
 }
 
